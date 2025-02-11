0560c64dc24275c3c81782dd23be11b69bfeb664
abrar0124 <ssgdaraz10@gmail.com>
2025-02-11 11:41:44 -0500

    bootstrap

---
+++ src/components/Footer.jsx
@@ -0,0 +1,17 @@
+function Footer() {
+  return (
+    <>
+      <footer class="py-5 text-center text-body-secondary bg-dark mt-5">
+        <p class="text-white">
+          Blog template built for
+          <a href="https://getbootstrap.com/">Coding Monkeys</a> by
+          <a href="https://twitter.com/mdo">@SabaMukhtar</a>.
+        </p>
+        <p class="mb-0">
+          <a href="#">Back to top</a>
+        </p>
+      </footer>
+    </>
+  );
+}
+export default Footer;
